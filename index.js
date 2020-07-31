b280bd9e85485f8212948501e51073e66a19fd10
raspyweather <raspyweather@gmail.com>
2020-07-31 17:45:54 +0200

    fixed typo

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const getThermalZones = () => readdir('/sys/class/thermal/')
 
 const readThermal = async (zone) => {
     const temperature = parseFloat((await readFile(`/sys/class/thermal/${zone}/temp`)).toString()) / 1000;
-    const name = (await readFile(`'/sys/class/thermal/${zone}/type`)).toString();
+    const name = (await readFile(`/sys/class/thermal/${zone}/type`)).toString();
     return { temperature, name };
 }
 
